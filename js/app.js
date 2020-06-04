863137033d19aa275b6042fbf9140da0ae828367
gkarumbi <gkarumbi@gmail.com>
2020-06-04 23:25:34 +0300

    Add redraw functions

--- js/app.js
+++ js/app.js
@@ -53,15 +53,59 @@ document.addEventListener('DOMContentLoaded',()=>{
 
 
 //create board
+cardArray.sort(()=> 0.5 -Math.random());
 
 const grid = document.querySelector('.grid');
+const resultDisplay = document.querySelector('#result');
+var cardsChosen =[];
+var cardsChosenID = [];
+var cardWons = [];
 function createBoard(){
     for (let i=0; i<cardArray.length; i++){
         var card = document.createElement('img');
         card.setAttribute('src','./images/blank.png');
         card.setAttribute('data-id',i);
-        //card.addEventListener('click',flipcard());
+        card.addEventListener('click',flipCard);
         grid.appendChild(card);
+
+    }
+}
+
+//check for matches
+function checkForMatch(){
+    var cards = document.querySelectorAll('img');
+    const optionOneID = cardsChosenID[0];
+    const optionTwoID = cardsChosenID[1];
+    if(cardsChosen[0]===cardsChosen[1]){
+        alert('You have found a match');
+        cards[optionOneID].setAttribute('src','images/white.png');
+        cards[optionTwoID].setAttribute('src','images/white.png');
+        cardWons.push(cardsChosen);
+
+    }else{
+        cards[optionOneID].setAttribute('src','images/blank.png');
+        cards[optionTwoID].setAttribute('src','images/blank.png');
+        alert('Sorry try again');
+
+    }
+    cardsChosen = [];
+    cardsChosenID = [];
+    resultDisplay.textContent = cardWons.length;
+    if(cardWons.length ===cardArray.length/2){
+        resultDisplay.textContent = 'Congratulations! You found them all';
+
+    }
+}
+
+//flip your card
+
+function flipCard(){
+    var cardID = this.getAttribute('data-id');
+    cardsChosen.push(cardArray[cardID].name);
+    cardsChosenID.push(cardID);
+    this.setAttribute('src', cardArray[cardID].img);
+    if (cardsChosen.length ===2){
+        setTimeout(checkForMatch, 500);
 
     }
 }
@@ -69,6 +113,8 @@ function createBoard(){
 
 
 createBoard();
+
+flipCard();
 
 
 
